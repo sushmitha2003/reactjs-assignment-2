2a431c5f126db5a7061aff396f17d19ce75b29a8
sushmitha <20b01a1248@svecw.edu.in>
2022-02-12 11:05:11 +0530

    assignment2 files from local repo to remote repo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+// import Text from "./Text.js";
+
+
+function Text()
+{
+     return(
+      <div class="ui cards">
+  <div class="card">
+    <div class="content">
+      <img class="right floated mini ui image" src={faker.image.image()} />
+      <div class="header">
+        Elliot Fu
+      </div>
+      <div class="meta">
+        Friends of Veronika
+      </div>
+      <div class="description">
+        Elliot requested permission to view your contact details
+      </div>
+    </div>
+    <div class="extra content">
+      <div class="ui two buttons">
+        <div class="ui basic green button">Approve</div>
+        <div class="ui basic red button">Decline</div>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="content">
+      <img class="right floated mini ui image" src={faker.image.image()}  />
+      <div class="header">
+        Jenny Hess
+      </div>
+      <div class="meta">
+        New Member
+      </div>
+      <div class="description">
+        Jenny wants to add you to the group <b>best friends</b>
+      </div>
+    </div>
+    <div class="extra content">
+      <div class="ui two buttons">
+        <div class="ui basic green button">Approve</div>
+        <div class="ui basic red button">Decline</div>
+      </div>
+    </div>
+  </div>
+</div>
+ )
+}
+
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <>
+      <Text />
+    </>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    document.getElementById("root")
+    );
